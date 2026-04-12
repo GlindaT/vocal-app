f3b301583aec28256f3bf0a4169b6083cd2cd08e
GlindaT <caminante.via@gmail.com>
2026-04-12 05:49:22 -0500

    Update script.js

--- script.js
+++ script.js
@@ -372,7 +372,6 @@ let studioRecordedBlob = null;
 let studioTrackFileName = "";
 let selectedVoiceBlob = null;
 let selectedVoiceId = null;
-let transcriptionSegments = [];
 let baseTranscriptionSegments = [];
 
 // ==========================================
