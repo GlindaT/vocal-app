8c4bbebbdb06b017191adcfdafc7c6825ada8f50
GlindaT <caminante.via@gmail.com>
2026-04-12 07:33:12 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1944,7 +1944,7 @@ function drawKaraokeMonitor(currentTime, currentFreq) {
                 ctx.textAlign = "center";
                 ctx.textBaseline = "middle";
                 ctx.fillText(seg.text, x + width / 2, y + 15);
-        });                               
+        }                               
               
     } else {
         // Si no hay segmentos, mostramos un mensaje
