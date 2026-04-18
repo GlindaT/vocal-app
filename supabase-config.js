d34cf5a7bf671a6167f89cd3c3ea71cc703eaba9
GlindaT <caminante.via@gmail.com>
2026-04-18 07:11:01 -0500

    Create supabase-config.js

---
+++ supabase-config.js
@@ -0,0 +1,4 @@
+const SUPABASE_URL = "https://xcsjmehnnwimbyhalleb.supabase.co";
+const SUPABASE_ANON_KEY = "sb_publishable_jUJGDmi8TB_umpxkcYUFXw_47bVXeDk";
+
+const supabaseClient = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
