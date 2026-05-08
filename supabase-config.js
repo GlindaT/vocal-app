8afb878ac3a3adaec692a473dde3be24a868b4e0
GlindaT <caminante.via@gmail.com>
2026-05-08 01:24:17 -0500

    Fix formatting in supabase-config.js

--- supabase-config.js
+++ supabase-config.js
@@ -1,4 +1,3 @@
 const SUPABASE_URL = "https://xcsjmehnnwimbyhalleb.supabase.co";
 const SUPABASE_ANON_KEY = "sb_publishable_jUJGDmi8TB_umpxkcYUFXw_47bVXeDk";
-
 const supabaseClient = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
