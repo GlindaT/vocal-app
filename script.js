741f63031c2f2aeaf74cfe1d96c89935ec7ffcf9
GlindaT <caminante.via@gmail.com>
2026-06-09 01:32:46 -0500

    Update script.js

--- script.js
+++ script.js
@@ -852,7 +852,6 @@ async function renderLibrary(filter = 'todos') {
     await loadTrackOptionsInStudio();
     await loadTrackOptionsInKaraoke();
     await loadTextOptionsInStudio();
-    await loadTextFromLibraryStudio();
     await loadSelectedTextFromLibraryStudio();
 
   } catch (error) {
