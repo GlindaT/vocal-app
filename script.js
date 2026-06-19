f74f88ef643215f832ed88c3aef1a8638815b8c7
GlindaT <caminante.via@gmail.com>
2026-06-19 06:53:58 -0500

    Update script.js

--- script.js
+++ script.js
@@ -948,7 +948,7 @@ async function renderLibrary(filter = 'todos') {
 
   try {
     // 1. Mejora de eficiencia: Si no es 'todos', usamos el índice de la DB
-    let filteredItems;
+    //let filteredItems;
     if (filter === 'todos') {
       filteredItems = await getAllLibraryItems();
     } else {
