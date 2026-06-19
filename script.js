c74dd26e0fb7e2474b6662e7920599415a7bb776
GlindaT <caminante.via@gmail.com>
2026-06-19 07:01:01 -0500

    Update script.js

--- script.js
+++ script.js
@@ -956,7 +956,7 @@ async function renderLibrary(filter = 'todos') {
       filteredItems = await getLibraryItemsByType(filter);
     }
     let library = await getAllLibraryItems();
-    //let filteredItems = filter !== 'todos' ? library.filter(item => item.type === filter) : library;
+    const filteredItems = filter !== 'todos' ? library.filter(item => item.type === filter) : library;
     
     container.innerHTML = "";
 
