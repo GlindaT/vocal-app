2d949986780d11fd120348a44aeab2f34ede0839
GlindaT <caminante.via@gmail.com>
2026-06-19 07:21:01 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1029,7 +1029,7 @@ function asignarEventosBiblioteca(filter) {
   document.querySelectorAll(".load-monitor-btn").forEach((btn) => {
     btn.addEventListener("click", async () => {
       const id = Number(btn.dataset.id);
-      const item = libraryItem.find(i => i.id === id);
+      const item = libraryItems.find(i => i.id === id);
 
       if (item && item.textoPlano) {
         const monitor = document.getElementById("lyricsText") || document.getElementById("miniMonitorTextArea");
