fb255cf8572cf107efd65ebe6b7d35192adbae5c
GlindaT <caminante.via@gmail.com>
2026-08-25 21:50:15 -0500

    Change R2 storage reference for file upload

--- cloudflare-worker.js
+++ cloudflare-worker.js
@@ -87,7 +87,7 @@ async function handleUpload(request, env, corsHeaders) {
 
   const safePath = cleanName;
 
-  await env.VOCAL_APP_R2.put(safePath, fileData, {
+  await env.VOCAL_APP_R2_UPLOAD.put(safePath, fileData, {
     httpMetadata: { contentType: mimeType },
   });
 
